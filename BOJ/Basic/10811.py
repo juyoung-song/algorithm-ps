41f80da54ed9709ba9908a0f8583f8011155ae1d
juyoung-song <skrhtnfk@gmail.com>
2023-12-13 23:52:19 +0900

    feat(10811): BOJ 바구니 뒤집기

---
+++ BOJ/Basic/10811.py
@@ -0,0 +1,17 @@
+# 바구니 갯수, 실행 횟수
+N, M = map(int, input().split())
+
+# 바구니 list 초기화
+basket = [k+1 for k in range(N)]
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    basket1 = basket[i-1:j]
+    basket1.reverse()
+    basket1 = list(basket1)
+    basket2 = basket[:i-1]
+    basket3 = basket[j:]
+    basket = basket2+basket1+basket3
+
+for k in range(len(basket)):
+    print(basket[k], end=' ')
